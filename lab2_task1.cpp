f63cec457600f0d1b6eed487c69768dceb295bbe
Nastya <natyachirvonaya@gmail.com>
2017-12-10 02:16:58 +0300

    lab2 a) points

---
+++ lab2_task1.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+const double eps = 1e-6;
+
+double f(double x, double y)
+{
+  return pow(x, 2) - 2 * pow(y, 2) - y * x + 2 * x - y + 1;
+}
+
+double g(double x, double y)
+{
+  return 2 * pow(x, 2) - pow(y, 2) + y * x + 3 * y - 5;
+}
+
+double df_dx(double x, double y)
+{
+  return 2 * x - y + 2;
+}
+
+double dg_dx(double x, double y)
+{
+  return 4 * x + y;
+}
+
+double df_dy(double x, double y)
+{
+  return -4 * y - x - 1;
+}
+
+double dg_dy(double x, double y)
+{
+  return -2 * y + x + 3;
+}
+int main()
+{
+
+  cout << "f(x, y) = x^2 - 2y^2 - xy + 2x - y + 1\n";
+  cout << "g(x, y) = 2x^2 - y^2 - yx + 3y - 5\n";
+
+  cout << "Brown method:\n";
+
+  double xk(0), x_k(0), qk(1), pk(1), yk(0);
+
+  while (max(abs(pk), abs(qk)) > eps) {
+    x_k = xk - f(xk, yk) / df_dx(xk, yk);
+    qk = g(x_k, yk) * df_dx(xk, yk) / (df_dx(xk, yk)*dg_dy(xk, yk) - df_dy(xk, yk)*dg_dx(xk, yk));
+    pk = (f(xk, yk) - qk*df_dy(xk, yk)) / df_dx(xk, yk);
+    xk -= pk;
+    yk -= qk;
+  }
+
+  cout << "x = " << xk << "\n" << "y = " << yk << "\n";
+
+  return 0;
+}
